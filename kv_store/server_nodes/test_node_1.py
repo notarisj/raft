717b693ed4e92e49d0af58755ba213e20bc96c13
Giannis Kalopisis <gianniskalopisis@gmail.com>
2023-06-20 11:41:37 +0300

    Add files of server nodes
    Simple files that start Key-Value server nodes.

---
+++ kv_store/server_nodes/test_node_1.py
@@ -0,0 +1,7 @@
+from kv_store.server.kv_server import KVServer
+
+if __name__ == '__main__':
+    kv_server = KVServer(_kv_server_host='localhost', _kv_server_port=9001,
+                         _raft_server_host='localhost', _raft_server_port=9010,
+                         _id=1, _server_list_file='/home/giannis-pc/Desktop/raft/kv_store/resources/serverFile.txt')
+    kv_server.start()
