be30413f3f4a6b51fae3e678f307db81058cc708
Giannis Kalopisis <gianniskalopisis@gmail.com>
2023-06-23 12:18:37 +0300

    API requester to communicate with RAFT

---
+++ kv_store/server/api_requester.py
@@ -0,0 +1,17 @@
+import requests
+
+
+class APIRequester:
+
+    def __init__(self, _host, _port):
+        self.host = _host
+        self.port = _port
+        self.url = f"http://{_host}:{_port}"
+
+    def post_append_entry(self, payload_json, _endpoint="/append_entries"):
+        send_url = self.url + _endpoint
+        response = requests.post(send_url, data=payload_json, headers={'Content-Type': 'application/json'})
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
